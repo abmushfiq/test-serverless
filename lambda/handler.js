125cbe120fcdbc4fe2255086177248ae3736024b
abmushfiq <abmushfiq@gmail.com>
2024-03-06 11:10:30 +0530

    test5

--- lambda/handler.js
+++ lambda/handler.js
@@ -1,15 +1,9 @@
 // handler.js
 
 module.exports.hello = async (event) => {
+  console.log("Hello from Lambda!");
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify({ message: "Hello from Lambda!" }),
   };
 };
